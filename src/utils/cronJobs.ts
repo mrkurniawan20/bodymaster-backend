8fbfb98fafea5f3f55dac2e5d60fd32c8d38aee2
NewUser <mraflikurniawan20@gmail.com>
2025-05-28 08:06:43 +0700

    Fix: Fixing the timezone into Jakarta for Cron

--- src/utils/cronJobs.ts
+++ src/utils/cronJobs.ts
@@ -3,25 +3,30 @@ import { prisma } from '../prisma/client';
 import cron from 'node-cron';
 
 export function CronJob() {
-  cron.schedule('0 0 * * *', async () => {
-    const now = new Date();
-    try {
-      prisma.$transaction(async (tx: Prisma.TransactionClient) => {
-        const memberExpired = await tx.member.updateMany({
-          where: { expireDate: { lte: now }, status: { not: 'INACTIVE' } },
-          data: { status: 'INACTIVE' },
-        });
-        if (memberExpired.count > 0) {
-          const addNotif = await tx.notifications.create({
-            data: {
-              content: `${memberExpired.count} members expired, click to see all expired member`,
-            },
+  cron.schedule(
+    '0 0 * * *',
+    async () => {
+      const now = new Date();
+      try {
+        prisma.$transaction(async (tx: Prisma.TransactionClient) => {
+          const memberExpired = await tx.member.updateMany({
+            where: { expireDate: { lte: now }, status: { not: 'INACTIVE' } },
+            data: { status: 'INACTIVE' },
           });
-        }
-        // console.log(`${memberExpired.count} members diactivated`);
-      });
-    } catch (error: any) {
-      console.log(error.message);
+          if (memberExpired.count > 0) {
+            const addNotif = await tx.notifications.create({
+              data: {
+                content: `${memberExpired.count} members expired, click to see all expired member`,
+              },
+            });
+          }
+        });
+      } catch (error: any) {
+        console.log(error.message);
+      }
+    },
+    {
+      timezone: 'Asia/Jakarta',
     }
-  });
+  );
 }
